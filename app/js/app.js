a086d9a55437deab6ecf7ffef30c0467649d1690
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-09-25 00:43:43 -0700

    Geolocation and API call are succesfull

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,63 @@
-var app = angular.module('travelApp', [])
-    .config(function () {
+$(document).ready(function () {
 
+    /* Update all the parameters for the API test*/
+    var params = {
+        key: 'mzjLtladPkAFANEEharRcAvY1h4ev9vo',
+        tagged: 'html',
+        site: 'mapquest',
+        order: 'desc',
+        sort: 'creation',
+        maxMatches: 10
+    };
+
+    // on clicking the Go button, search the map quest API
+    $("#search-button").click(function () {
+        // make ajax call
+        var result = $.ajax({
+
+                url: "http://www.mapquestapi.com/search/v2/radius?key=mzjLtladPkAFANEEharRcAvY1h4ev9vo&maxMatches=4&origin=" + pos.lat + "," + pos.lng,
+                type: "GET"
+            })
+            /* if the call is successful (status 200 OK) show results */
+            .done(function (result) {
+                console.log(pos);
+                console.log(result.searchResults);
+                // get the search list from html
+                var searchList = $('#search-details');
+                // remove all prev. elements from search list
+                searchList.empty();
+                // getting all search resuts
+                var searchResults = result.searchResults;
+                // for each result we get
+                searchResults.forEach(function (place) {
+                    console.log(place.name);
+                    // create new html elements
+                    var el = $("<li>");
+                    // set the element text to be the result name
+                    $(el).text(place.name);
+                    // applied the result to the search list
+                    searchList.append(el);
+
+                });
+            })
+
+
+
+        /* if the call is NOT successful show errors */
+        .fail(function (jqXHR, error, errorThrown) {
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
     });
+    /*function geocode(place, callback) {
+        var url = 'http://open.mapquestapi.com/search?format=json&q=' + encodeURIComponent(place);
+        // requires jquery for ajax
+        jQuery.getJSON(url, function(data) {
+            if (data.length > 0) {
+                callback(null, { lon: parseFloat(data[0].lon), lat: parseFloat(data[0].lat) })
+            } else {
+                callback(null, null)
+            }
+        });*/
+});
--- app/js/library.js
+++ app/js/library.js
@@ -1,24 +1,60 @@
+/*
 angular.module('travelLibrary', [])
 
-//Initialize constants
-//.constant('GOOGLE_API_PREFIX', 'https://www.googleapis.com/geolocation/v1/geolocate')
-.constant('GOOGLE_API_KEY', 'AIzaSyBC7EtYRUIx6NPiiMPvFwIpTMn8OuFKP4Y')
-
-//Accept the current location as a parameter
-.factory('geoLocation', ['$http', 'GOOGLE_API_PREFIX', 'GOOGLE_API_KEY', function ($http, GOOGLE_API_PREFIX, GOOGLE_API_KEY) {
-    var params = {
-        apiKey: 'GOOGLE_API_KEY',
-        address: 'location'
-    }
-
-    return $http({
-        method: 'GET',
-        url: 'GOOGLE_API_PREFIX',
-        params: params
+var lat = -33.8688
+var lng = 151.2195
+$.ajax({
+        url: 'http://www.mapquestapi.com/directions/v2/route?',
+        dataType: 'jsonP',
+        crossDomain: true,
+        data: {
+            key: 'mzjLtladPkAFANEEharRcAvY1h4ev9vo',
+            origin: lat + lng,
+            maxMatches: 10
+        }
     })
+    */
 
-    //callback function goes here
-    .then(function () {
-        return response.data.results.location;
-    });
-}])
+/*
+//initiliaze constants
+.constant('MAPQUEST_API_PREFIX', 'http://www.mapquestapi.com/directions/v2/route')
+    .constant('MAPQUEST_API_KEY', 'mzjLtladPkAFANEEharRcAvY1h4ev9vo')
+
+var request = {
+    callback: 'JSON_CALLBACK',
+};
+
+$.ajax({
+    url: "http://www.mapquestapi.com/directions/v2/route",
+    data: request,
+    dataType: "jsonp",
+    type: "GET",
+})
+*/
+//Accept the location as a parameter and returns the tourist attractions for that location
+/*.factory('localAttractions', ['$http', 'TRIPADVISOR_API_PREFIX', 'TRIPADVISOR_API_KEY', function ($http, TRIPADVISOR_API_PREFIX, TRIPADVISOR_API_KEY) {
+
+            var urlBase = "http://api.tripadvisor.com/api/partner/2.0/map/42.33141,-71.099396?";
+
+            return {
+
+                getData: function (lat, lng) {
+                    var url = urlBase + "localInfoJSON";
+                    var request = {
+                        callback: 'JSON_CALLBACK',
+                    };
+
+                    $http({
+                        method: 'JSONP',
+                        url: url + lat ',' + lng,
+                        params: request,
+                        cache: true
+                    })
+
+                    //callback function goes here
+                    .then(function (response) {
+                        return response.data.results.location;
+
+                    });
+                }
+            }])*/
