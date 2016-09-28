ffcc74aaab81fb73836cdea8c7fa5ccb50991ac1
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-09-27 19:36:29 -0700

    working on ng-geolocation

--- app/js/app.js
+++ app/js/app.js
@@ -1,57 +1,3 @@
-/*$(document).ready(function () {
-
-    /* Update all the parameters for the API test*/
-/*var params = {
-        key: 'mzjLtladPkAFANEEharRcAvY1h4ev9vo',
-        tagged: 'html',
-        site: 'mapquest',
-        order: 'desc',
-        sort: 'creation',
-        maxMatches: 10
-    };
-
-    // on clicking the Go button, search the map quest API
-    $("#search-button").click(function () {
-        // make ajax call
-        var result = $.ajax({
-
-                url: "http://www.mapquestapi.com/search/v2/radius?key=mzjLtladPkAFANEEharRcAvY1h4ev9vo&maxMatches=4&origin=" + pos.lat + "," + pos.lng,
-                type: "GET"
-            })
-            //if the call is successful (status 200 OK) show results
-            .done(function (result) {
-                console.log(pos);
-                console.log(result.searchResults);
-                // get the search list from html
-                var searchList = $('#search-details');
-                // remove all prev. elements from search list
-                searchList.empty();
-                // getting all search resuts
-                var searchResults = result.searchResults;
-                // for each result we get
-                searchResults.forEach(function (place) {
-                    console.log(place.name);
-                    // create new html elements
-                    var el = $("<li>");
-                    // set the element text to be the result name
-                    $(el).text(place.name);
-                    // applied the result to the search list
-                    searchList.append(el);
-
-                });
-            })
-
-        //if the call is NOT successful show errors
-        .fail(function (jqXHR, error, errorThrown) {
-            console.log(jqXHR);
-            console.log(error);
-            console.log(errorThrown);
-        });
-    });
-
-});
-*/
-
 var app = angular.module('myApp', ["ngGeolocation"]);
 
 app.controller('geolocCtrl', ['$geolocation', '$scope', function ($geolocation, $scope) {
@@ -59,7 +5,7 @@ app.controller('geolocCtrl', ['$geolocation', '$scope', function ($geolocation, 
         timeout: 60000
     }).then(function (position) {
         $scope.myPosition = position;
-        console.log($scope.myPosition);
+        console.log("inside controller", $scope.myPosition);
     });
 }]);
 
@@ -72,41 +18,30 @@ function attractionsController($scope, $http) {
     $scope.isSearching = false;
     $scope.searchList = 0;
 
-    $scope.search = function () {
+    $scope.search = function (geolocatedPosition) {
+
         $scope.submit = function () {
             $scope.isSearching = true;
         }
 
         $http({
                 method: "GET",
-                url: "http://www.mapquestapi.com/search/v2/radius?key=mzjLtladPkAFANEEharRcAvY1h4ev9vo&maxMatches=4&origin=" + pos.lat + "," + pos.lng,
-                params: {
-                    key: "mzjLtladPkAFANEEharRcAvY1h4ev9vo",
-                    text: $scope.searchPlace,
-                    format: "json",
-                    per_page: 10,
-                    tagged: "html",
-                    site: "MapQuest",
-                    order: "desc",
-                    sort: "creation"
-                }
-
+                url: "http://www.mapquestapi.com/search/v2/radius?key=mzjLtladPkAFANEEharRcAvY1h4ev9vo&maxMatches=4&origin=39.750307,-104.999472",
+                //url: "http://www.mapquestapi.com/search/v2/radius?key=mzjLtladPkAFANEEharRcAvY1h4ev9vo&maxMatches=4&origin=" + geolocatedPosition,
+                params: {}
             })
             .success(function (data) {
-                $scope.searchResults = data;
-                console.log(data);
-                /*$scope.isSearching = false;
-                // helper function called
-                $scope.searchList = data.photos.total;
-                console.log(data);
-                */
+                if (data.resultsCount > 0) {
+                    $scope.searchResults = data.searchResults;
+                } else {
+                    alert("Api call returns 200 but no results");
+                }
+                console.log($scope.searchResults);
 
 
             }),
             function (error) {
-                console.log('error!');
-                $scope.isSearching = true;
+                console.log('Api call returns nothing');
             };
-
     };
 };
